fd7831abf46c6507e29ff54161513e92b108180d
Zunaib Imtiaz <zunaib60@gmail.com>
2024-06-26 15:24:03 +0500

    Test Deployment

--- src/App.tsx
+++ src/App.tsx
@@ -108,7 +108,7 @@ const App = () => {
   };
 
   const onUIReady = () => {
-    console.log('on UI Ready');
+    console.log('on UI Ready Edit');
     const welcomeData: WelcomeData =
       window.emperia?.data.ui.uiConfig['welcome'] || fallbackData.data.ui.uiConfig['welcome'];
     const instructionsData: InstructionsData =
