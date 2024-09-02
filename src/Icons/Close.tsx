6abc84166c21d6df4d4c2f0fac56069ef23962ff
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2024-09-02 09:25:48 +0100

    fix instruction breaking on mobile

---
+++ src/Icons/Close.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Icon, IconProps } from '@chakra-ui/icons';
+
+export const Close = (props: IconProps) => (
+  <Icon width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="M9.5 0.5L0.5 9.5M9.5 9.5L0.5 0.5" strokeLinecap="round" strokeLinejoin="round" />
+  </Icon>
+);
