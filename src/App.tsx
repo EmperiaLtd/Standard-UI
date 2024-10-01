a058be506151a96656d666dacb0b118ab50ca01f
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2024-10-01 14:03:18 +0100

    Add event instead of calling krpano directly from standard ui

--- src/App.tsx
+++ src/App.tsx
@@ -240,7 +240,22 @@ const App = () => {
         activeSound={activeSound}
         setActiveScene={(scene) => {
           setActiveScene(scene);
-          window?.emperia?.experience?.krpano.call(`loadscene(${scene}, null, MERGE, BLEND(0.5));`);
+          // window?.emperia?.experience?.krpano.call(`loadscene(${scene}, null, MERGE, BLEND(0.5));`);
+
+          const iframe = document.getElementById('experience-container') as HTMLIFrameElement;
+
+          if (iframe) {
+            const iframeWindow = iframe.contentWindow;
+            if (iframeWindow) {
+              iframeWindow.postMessage(
+                {
+                  type: 'loadScene',
+                  scene,
+                },
+                '*',
+              );
+            }
+          }
         }}
         setActiveLang={(lang) => changeLanguage(lang)}
         setActiveSound={(sound) => setActiveSound(sound)}
